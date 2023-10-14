5c1555ff144f8d6b419894ebaee04efbb5137042
boost1ne <boustane.chaimaa@gmail.com>
2023-10-14 13:23:47 -0700

    update

--- handle_string.c
+++ handle_string.c
@@ -1,34 +1,24 @@
 #include "main.h"
 /**
-*handle_string - Function to handle the %s format specifier
-*@str: pointer to string.
-*
-*Return: void.
+ * handle_string - prints a string
+ * @str: string to be printer
+ * Return: length of the string
 */
 int handle_string(char *str)
 {
+
 	int count = 0;
 
 	if (str == NULL)
 	{
-		char *n = "(null)";
-
-		while (*n)
-		{
-			print_char(*n);
-			n++;
-			count++;
-		}
-		return (count);
+		handle_string("null");
+		return (6);
 	}
-	else
+	while (*str)
 	{
-		while (*str)
-		{
-			print_char(*str);
-			str++;
-			count++;
-		}
+		print_char(*str);
+		str++;
+		count++;
 	}
 	return (count);
 }
--- hundle_di.c
+++ hundle_di.c
@@ -4,17 +4,47 @@
  * @num: take argumenet int
  *
  * Return: void
- */
+**/
 int handle_int(int num)
 {
-	char buffer[1024];
-	int len = snprintf(buffer, sizeof(buffer), "%d", num);
+	int i;
+	char buffer[12];
+	int count = 0;
+	int s, e;
 
-	if (len > 0)
+	if (num < 0)
 	{
-		write(1, buffer, len);
-		return (len);
+		print_char('-');
+		num = -num;
 	}
-	else
-		return (-1);
+	if (num == 0)
+	{
+		buffer[count] = '0';
+		count++;
+	}
+	while (num > 0)
+	{
+		int j = num % 10;
+
+		buffer[count] = j + '0';
+		count++;
+		num = num / 10;
+	}
+	s = 0;
+	e = count - 1;
+	while (s < e)
+	{
+		char aux = buffer[s];
+
+		buffer[s] = buffer[e];
+		buffer[e] = aux;
+		s++;
+		e--;
+	}
+	buffer[count] = '\0';
+	for (i = 0; i < count; i++)
+	{
+		print_char(buffer[i]);
+	}
+	return (count);
 }
